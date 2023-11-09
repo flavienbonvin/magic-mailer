3fdbace47648aa17325eeb1519b09c8fe880722b
Flavien Bonvin <flavien.bonvin@pm.me>
2023-11-09 22:23:19 +0100

    Fix dark login background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-gradient-to-b from-stone-50 to-green-50">
+    <div className="flex min-h-screen w-full items-center justify-center bg-gradient-to-b from-stone-50 to-green-50 dark:from-stone-900 dark:to-stone-950">
       <Card className="w-4/6 p-6">
         <CardHeader>
           <CardTitle>Login to access</CardTitle>
