28964bab2a5ccad1cce53757de8647d7a4dfb7d4
Flavien Bonvin <flavien.bonvin@pm.me>
2023-11-09 22:34:19 +0100

    Improve login layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-gradient-to-b from-stone-50 to-green-50 dark:from-stone-900 dark:to-stone-950">
-      <Card className="w-4/6 p-6">
+      <Card className="3xl:w-1/6 w-5/6 p-6 md:w-2/3 lg:w-2/6">
         <CardHeader>
           <CardTitle>Login to access</CardTitle>
         </CardHeader>
