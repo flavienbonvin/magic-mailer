a77daf736c5e81c42dd4abaf261bceb92562d055
Flavien Bonvin <flavien.bonvin@pm.me>
2023-11-10 23:26:29 +0100

    Properly fix theme provider issue

--- src/components/atoms/theme-switcher.tsx
+++ src/components/atoms/theme-switcher.tsx
@@ -2,25 +2,16 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 
 const ThemeSwitcher = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
 
   const handleClick = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   const child = theme === "dark" ? <Sun /> : <Moon />;
-
   return (
     <Button onClick={handleClick} size="icon" variant="ghost" type="button">
       {child}
