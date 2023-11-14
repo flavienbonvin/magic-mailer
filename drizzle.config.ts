4c3ea8b8340eb65664c0a984c72c72d1d70af978
Flavien Bonvin <flavien.bonvin@pm.me>
2023-11-14 22:55:45 +0100

    Remove dotenv since bun reads env file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-
-config({ path: ".env.development.local" });
 
 export default defineConfig({
   schema: "./src/data/schema.ts",
