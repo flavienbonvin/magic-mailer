f4a706e356183a68c86bab17fa60f340cff06526
Flavien Bonvin <flavien.bonvin@pm.me>
2023-11-26 10:38:47 +0100

    Remove outdated attendee

--- src/data/models/attendee.ts
+++
@@ -1,9 +0,0 @@
-// TODO merge this with Zod type to avoid duplication
-interface Attendee {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
-export default Attendee;
