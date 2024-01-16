4fca08e8a17c3a5079e6321acf242eebf6aefb71
Flavien Bonvin <flavien.bonvin@pm.me>
2024-01-16 22:19:12 +0100

    x

--- src/app/(logged)/admin/page.tsx
+++ src/app/(logged)/admin/page.tsx
@@ -1,17 +1,18 @@
 import AdminsTable from "@/components/features/admin/admins-table";
 import H1 from "@/components/typography/h1";
+import { AUTH_COOKIE } from "@/constants";
 import { getUser } from "@/data/actions/user";
-import { getCookie } from "@/lib/cookie";
-import { log } from "console";
+import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const cookie = getCookie();
-  log({ cookie });
-  const user = await getUser(cookie?.value);
+  const cookie = cookies().get(AUTH_COOKIE)?.value;
+  console.log({ cookie });
+
+  const user = await getUser(cookie);
   console.log({ user });
 
   if (!user?.isAdmin) {
--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -5,6 +5,9 @@ import { cookies } from "next/headers";
 
 const cookieOption = {
   maxAge: AUTH_COOKIE_MAX_AGE,
+  httpOnly: true,
+  path: "/",
+  secure: true,
 };
 
 export const hasValidCookie = () => {
