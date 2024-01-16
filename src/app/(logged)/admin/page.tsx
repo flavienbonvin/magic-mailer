f17b539ab9b3c7456956a570c3ee9b40568543b5
Flavien Bonvin <flavien.bonvin@pm.me>
2024-01-16 22:31:55 +0100

    x

--- src/app/(logged)/admin/page.tsx
+++ src/app/(logged)/admin/page.tsx
@@ -1,11 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import AdminsTable from "@/components/features/admin/admins-table";
 import H1 from "@/components/typography/h1";
 import { AUTH_COOKIE } from "@/constants";
 import { getUser } from "@/data/actions/user";
 import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
-
-export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const cookie = cookies().get(AUTH_COOKIE)?.value;
