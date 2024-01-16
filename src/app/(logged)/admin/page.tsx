a987715c1fd84705d029782999ed007d347fcd11
Flavien Bonvin <flavien.bonvin@pm.me>
2024-01-16 22:10:09 +0100

    x

--- src/app/(logged)/admin/page.tsx
+++ src/app/(logged)/admin/page.tsx
@@ -2,13 +2,17 @@ import AdminsTable from "@/components/features/admin/admins-table";
 import H1 from "@/components/typography/h1";
 import { getUser } from "@/data/actions/user";
 import { getCookie } from "@/lib/cookie";
+import { log } from "console";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const cookie = getCookie();
+  log({ cookie });
   const user = await getUser(cookie?.value);
-  console.log(user);
+  console.log({ user });
 
   if (!user?.isAdmin) {
     return notFound();
